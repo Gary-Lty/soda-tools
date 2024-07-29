4881ff6dcb9cf04018b4fd63fc6c4869f87db1da
gary <987918453@qq.com>
2024-07-29 16:07:16 +0800

    add：添加静态变量配置，和控制台

---
+++ 控制台-静态变量配置/Scripts/Game/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ConfigVar.Init();
+            Console.Init();
+        }
+
+        private void Update()
+        {
+            Console.ConsoleUpdate();
+        }
+    }
+}
