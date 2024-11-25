45cac2336bf8ba32e0bef341383d66f4db8f35b5
gary <987918453@qq.com>
2024-11-25 17:29:58 +0800

    添加simpale ui framework

---
+++ plugins/MoleMole.UserInerfaceFramework/runtime/Singleton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ *	
+ *  Singleton
+ *
+ *	by Xuanyi
+ *
+ */
+
+namespace MoleMole
+{
+    public class Singleton<T> where T : class
+    {
+        /*	Instance	*/
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Create();
+                }
+
+                return _instance;
+            }
+        }
+
+
+        public static void Create()
+        {
+            _instance = (T)Activator.CreateInstance(typeof(T), true);
+        }
+        
+        
+        /* Static constructor	*/
+        static Singleton()
+        {
+            return;
+        }
+
+        /*	Destroy	*/
+        public static void Destroy()
+        {
+
+            _instance = null;
+
+            return;
+        }
+    }
+}
